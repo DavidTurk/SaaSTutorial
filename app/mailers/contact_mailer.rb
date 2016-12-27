61d2f78d42453fa2151622110e7a0276da6105de
David Turk <david.turk@outlook.com>
2016-12-27 02:40:43 +0000

    Fixed contact mailer up

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'lordturk@gmail.com'
 
-    def contact_email
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
